ded9898aad0b1de64cc79549cc8cba177f815fce
Jhon Cruz <jhonlcp1997>
2022-01-06 12:36:29 -0500

    U 0.9.4.2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("Estoy empezando");
+
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector("#header-center-right");
+
+menu.onclick=()=>{
+    
+    busqueda.classList.remove('fa-times');
+    formBusqueda.classList.remove('active1');
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = ()=>{
+    busqueda.classList.remove('fa-times');
+    formBusqueda.classList.remove('active1');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
