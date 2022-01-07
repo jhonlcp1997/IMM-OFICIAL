b1f7bf7853b040727af09eee84f2f41fba42816a
Jhon Cruz <jhonlcp1997>
2022-01-07 10:21:02 -0500

    style 3.3 header menu boton

--- js/index.js
+++ js/index.js
@@ -3,16 +3,11 @@ let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector("#header-center-right");
 
 menu.onclick=()=>{
-    
-    busqueda.classList.remove('fa-times');
-    formBusqueda.classList.remove('active1');
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
 window.onscroll = ()=>{
-    busqueda.classList.remove('fa-times');
-    formBusqueda.classList.remove('active1');
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
