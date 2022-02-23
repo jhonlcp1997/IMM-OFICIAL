260144a695b68fb3e835b1e6f6a1c124abe1e879
Jhon Cruz <jhonlcp1997>
2022-02-23 15:51:39 -0500

    fixed  add content info-date

--- js/carrusel-egi-emi.js
+++ js/carrusel-egi-emi.js
@@ -20,10 +20,10 @@ window.addEventListener("resize", function () {
     height2 = slider.clientHeight;
     console.log(`Este es el height 1 ${height}`);
     console.log(`Este es el height 2 ${height2}`);
-    
-    if(height >250 ){
+
+    if (height > 250) {
         console.log("Salio del if");
-        for(let i = 0; i < sliderIndividual; i++){
+        for (let i = 0; i < sliderIndividual; i++) {
             sliderIndividual[i].style["height"] = "290px";
         }
     }
@@ -46,7 +46,7 @@ function slides() {
         children[7].style.background = "rgba(7, 17, 27, 0.2)";
         children[0].style.background = "#007cc3";
 
-        sliderIndividual[contador2].style["height"] = (height -50) + "px";
+        sliderIndividual[contador2].style["height"] = (height - 50) + "px";
         sliderIndividual[contador2].classList.remove('active');
 
         sliderIndividual[0].style["height"] = (height + 30) + "px";
@@ -78,12 +78,12 @@ function slides() {
     if (contador == (sliderIndividual.length - 3)) {
         setTimeout(function () {
             sliderIndividual[0].style["height"] = (height) + "px";
-            sliderIndividual[8].style["height"] = (height-5) + "px";
+            sliderIndividual[8].style["height"] = (height - 5) + "px";
             sliderIndividual[contador2].classList.remove('active');
 
             children[7].style.background = "rgba(7, 17, 27, 0.2)";
             children[0].style.background = "#007cc3";
-            
+
             slider.style.transform = "translate(0px)";
             slider.style.transition = "transform 0s";
             contador = 1;
@@ -93,12 +93,59 @@ function slides() {
 }
 
 // *============ CLICK EN PUNTOS ==========
+// function toggleStudy() {
+//     console.log("Entro en el toggleStudy");
+
+//     for (let k = 0; k < puntos.length; k++) {
+//         puntos[k].onclick = () => {
+//             // console.log("Entro al click de puntos");
+//             if (k === 0) {
+//                 slider.style.transform = "translate(0px)";
+//                 slider.style.transition = "transform 0s";
+
+//                 sliderIndividual[contador].style["height"] = (height) + "px";
+//                 sliderIndividual[contador].classList.remove('active');
+//                 console.log(`El contador borrado esta en ${contador}`);
+//                 children[contador2].style.background = "rgba(7, 17, 27, 0.2)";
+//                 console.log(`El contador que borrara el punto esta en ${contador2}`);
+
+//                 children[0].style.background = "#007cc3";
+//                 contador = 1;
+//                 contador2 = 0;
+//             }
+//             else {
+//                 slider.style.transform = "translate(" + (-210 * (k - 1)) + "px)";
+//                 slider.style.transition = "transform 1s";
+
+//                 sliderIndividual[contador - 1].style["height"] = (height) + "px";
+//                 sliderIndividual[contador - 1].classList.remove('active');
+//                 console.log(`El contador borrado esta en ${contador}`);
+//                 children[contador2 - 1].style.background = "rgba(7, 17, 27, 0.2)";
+//                 console.log(`El contador que borrara el punto esta en ${contador2}`);
+
+//                 sliderIndividual[k].style["height"] = (height + 30) + "px";
+//                 sliderIndividual[k].classList.toggle('active');
+//                 children[k].style.background = "#007cc3";
+//                 contador = k + 1;
+//                 contador2 = k;
+//             }
+//         }
+//     }
+// }
+
 function toggleStudy() {
-    console.log("Entro en el toggleStudy");
+    for (let i = 0; i < puntos.length; i++) {
+        puntos[i].style.background = "rgba(7, 17, 27, 0.2)";
+    }
+
+    console.log("Entro al togglestudy 2");
+    this.style["background"] = "#007cc3";
 
     for (let k = 0; k < puntos.length; k++) {
-        puntos[k].onclick = () => {
-            // console.log("Entro al click de puntos");
+        console.log("Hizo el for");
+        console.log(puntos[k].style["background"]);
+        if (puntos[k].style["background"] == "rgb(0, 124, 195)") {
+            console.log("Hay considencia en color azul");
             if (k === 0) {
                 slider.style.transform = "translate(0px)";
                 slider.style.transition = "transform 0s";
@@ -129,8 +176,10 @@ function toggleStudy() {
                 contador = k + 1;
                 contador2 = k;
             }
+
         }
     }
+
 }
 
 puntos.forEach((de) => {
