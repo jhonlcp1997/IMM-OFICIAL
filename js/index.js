bec8f957cae10239193a5748a546570750dbdf27
Jhon Cruz <jhonlcp1997>
2022-03-02 10:02:36 -0500

    menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
+// // *js solo para abrir el boton menu
+// let menu = document.querySelector("#menu-btn");
+// let navbar = document.querySelector("#header-center-right");
+
+// menu.onclick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
+
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
+
 // *js solo para abrir el boton menu
 let menu = document.querySelector("#menu-btn");
-let navbar = document.querySelector("#header-center-right");
+let navbar = document.querySelector(".header-menu");
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -11,4 +25,3 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
