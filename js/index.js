7ea38d64f790ae7b549e2a898da35b6282a332a2
Jhon Cruz <jhonlcp1997>
2022-03-07 14:51:46 -0500

    till header-footer

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 // *js solo para abrir el boton menu
-let menu = document.querySelector("#menu-btn");
-let navbar = document.querySelector("#header-center-right");
+// let menu = document.querySelector("#menu-btn");
+// let navbar = document.querySelector("#header-center-right");
+
+// menu.onclick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
+
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
+
+let menu = document.querySelector(".header-right");
+let navbar = document.querySelector(".header-menu");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
+    menu.classList.toggle('active');
     navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
+    menu.classList.remove('active');
     navbar.classList.remove('active');
 }
-
