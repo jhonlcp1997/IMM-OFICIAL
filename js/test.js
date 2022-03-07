7278124c8172e94742284c06b57819dafe7dcaf5
Jhon Cruz <jhonlcp1997>
2022-03-07 17:00:41 -0500

    till test js

--- js/test.js
+++ js/test.js
@@ -1,70 +1,8 @@
-let slider = document.querySelector(".slider-contenedor2");
-let sliderIndividual = document.querySelectorAll(".characteres_all");
-let children = document.getElementById("points").children;
-let puntos = document.querySelectorAll(".pt");
-let contador = 1;
-let contador2 = 0;
-// estas variables estarn porseacaso por que no se de ninguna de estas
-let width = sliderIndividual[0].clientWidth;
-let height = sliderIndividual[0].clientHeight;
-let intervalo = 2000;
+let sliders_g =  document.querySelector("slider-contenedor-g");
+let contenido_sliders_g = document.querySelectorAll(".contenido-slider-g");
 
-window.addEventListener("resize", function () {
-    width = sliderIndividual[0].clienteWidth;
-    height = sliderIndividual[0].clientHeight;
-})
+let width_1 = sliders_g.clientHeight;
+let width_2 = contenido_sliders_g[0].clientWidth;
 
-setInterval(function () {
-    slides();
-}, intervalo);
-
-function slides() {
-    // slider.style.transform = "translate("+((-width*contador))+"px)";
-    slider.style.transform = "translate(" + (-210 * contador2) + "px)";
-    slider.style.transition = "transform 1s";
-
-    if (contador > 8) {
-        children[7].style.background = "rgba(7, 17, 27, 0.2)";
-        children[0].style.background = "#007cc3";
-
-        sliderIndividual[contador2].style["height"] = (height) + "px";
-        sliderIndividual[contador2].classList.remove('active');
-
-        sliderIndividual[0].style["height"] = (height + 30) + "px";
-        sliderIndividual[0].classList.toggle('active');
-
-    } else if (contador === 1) {
-        sliderIndividual[contador].style["height"] = (height + 30) + "px";
-        sliderIndividual[contador].classList.toggle('active');
-        children[contador2].style.background = "#007cc3";
-    }
-
-    else {
-        sliderIndividual[contador].style["height"] = (height + 30) + "px";
-        sliderIndividual[contador].classList.toggle('active');
-
-        sliderIndividual[contador2].style["height"] = (height) + "px";
-        sliderIndividual[contador2].classList.remove('active');
-
-        children[contador2 - 1].style.background = "rgba(7, 17, 27, 0.2)";
-
-        children[contador2].style.background = "#007cc3";
-    }
-
-    contador++;
-    contador2++;
-
-    if (contador == (sliderIndividual.length - 3)) {
-        setTimeout(function () {
-            sliderIndividual[0].style["height"] = (height) + "px";
-            sliderIndividual[contador2].classList.remove('active');
-            children[7].style.background = "rgba(7, 17, 27, 0.2)";
-            children[0].style.background = "#007cc3";
-            // children[7].style.background = "rgba(7, 17, 27, 0.2)";
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            contador = 1;
-            contador2 = 0;
-        }, 1500)
-    }
-}
+console.log(width_1);
+console.log(width_2);
