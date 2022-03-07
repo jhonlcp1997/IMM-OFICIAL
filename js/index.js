40bd7d40cd64192d6ddce1fc9865b8532bd680d6
Jhon Cruz <jhonlcp1997>
2022-03-07 16:37:51 -0500

    fixed referencia

--- js/index.js
+++ js/index.js
@@ -14,13 +14,22 @@
 
 let menu = document.querySelector(".header-right");
 let navbar = document.querySelector(".header-menu");
+let general = document.querySelector(".slider-general");
 
 menu.onclick = () => {
+    general.classList.toggle('active');
     menu.classList.toggle('active');
     navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
+    general.classList.remove('active');
+    menu.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+window.onpointermove= () => {
+    general.classList.remove('active');
     menu.classList.remove('active');
     navbar.classList.remove('active');
 }
