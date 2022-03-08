f2788be38d164126b2204c8cecefdf1c611bb959
Jhon Cruz <jhonlcp1997>
2022-03-08 09:04:48 -0500

    fixed referencia

--- js/test.js
+++ js/test.js
@@ -1,19 +1,30 @@
-let slider_general = document.querySelector(".slider-general");
-let sliders_g =  document.querySelector("slider-contenedor-g");
+// let slider_general = document.querySelector(".slider-general");
+let sliders_g =  document.querySelector(".slider-contenedor-g");
 let contenido_sliders_g = document.querySelectorAll(".contenido-slider-g");
 
-let width_1 = slider_general.scrollWidth;
+let width_1 = sliders_g.scrollWidth;
 let width_2 = contenido_sliders_g[0].clientWidth;
+
 let contador_g = 0;
 
-console.log(width_1);
-console.log(width_2);
+// console.log(width_1);
+// console.log(width_2);
+// console.log(sliders_g);
 
-setInterval(function (){
-    slidesGeneral();
-}, 4000);
+// setInterval(function (){
+//     slidesGeneral();
+// }, 4000);
 
-function slidesGeneral(){
-    sliders_g.style.transform = "translate(200px)";;
-    contenido_sliders_g[1];
+// function slidesGeneral(){
+//     sliders_g.style.transform = "translate("+(-width_2 * contador_g)+"px)";
+//     contador_g++;
+
+//     if(contador_g == contenido_sliders_g.length){
+//         contador_g = 0;
+//     }
+// }
+
+window.onpointermove = () =>{
+    console.log("Se movio");
+    console.log(contenido_sliders_g);
 }
