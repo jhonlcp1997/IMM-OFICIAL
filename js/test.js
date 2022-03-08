181570b628f7ebb26c20e692982654049948e547
Jhon Cruz <jhonlcp1997>
2022-03-08 08:41:50 -0500

    ad

--- js/test.js
+++ js/test.js
@@ -1,8 +1,19 @@
+let slider_general = document.querySelector(".slider-general");
 let sliders_g =  document.querySelector("slider-contenedor-g");
 let contenido_sliders_g = document.querySelectorAll(".contenido-slider-g");
 
-let width_1 = sliders_g.clientHeight;
+let width_1 = slider_general.scrollWidth;
 let width_2 = contenido_sliders_g[0].clientWidth;
+let contador_g = 0;
 
 console.log(width_1);
 console.log(width_2);
+
+setInterval(function (){
+    slidesGeneral();
+}, 4000);
+
+function slidesGeneral(){
+    sliders_g.style.transform = "translate(200px)";;
+    contenido_sliders_g[1];
+}
