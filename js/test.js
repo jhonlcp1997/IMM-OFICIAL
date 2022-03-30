d3b77d8961a09dcfdcb7315295ee96f030e98fd4
Jhon Cruz <jhonlcp1997>
2022-03-30 14:36:58 -0500

    add-lines test.js

--- js/test.js
+++ js/test.js
@@ -12,3 +12,11 @@ let script ={
 
 console.log(script[2]);
 
+let texto =" Hola Soy tu amigo y enemigo a la vez guajajajaja";
+let texto2= "Página sin contenido";
+let texto3 = "what are you doing, what the fuck broo...";
+
+const hablar = (texto)=>speechSynthesis.
+speak(new SpeechSynthesisUtterance(texto));
+
+hablar(texto);
