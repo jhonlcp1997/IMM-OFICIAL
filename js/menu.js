c5230ec45530f4a388c922c188e84f03af16cd6a
Jhon Cruz <jhonlcp1997>
2022-03-31 08:41:41 -0500

    creation menu.js

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+// *js solo para abrir el boton menu
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector("#header-center-right");
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
