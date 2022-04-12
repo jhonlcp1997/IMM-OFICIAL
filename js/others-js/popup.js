d18990a755641257e01c8d670acb47b90c985ec2
Jhon Cruz <jhonlcp1997>
2022-04-12 13:50:05 -0500

    fixed referencia popup

--- js/others-js/popup.js
+++ js/others-js/popup.js
@@ -1,6 +1,11 @@
 const $popup = document.querySelector(".popups");
 const $icon_close = document.getElementById("icon_close");
+const $btnClose = document.getElementById("btn_popup");
 
 $icon_close.addEventListener("click", () =>{
+    $popup.classList.toggle("close");
+})
+
+$btnClose.addEventListener("click", () =>{
     $popup.classList.toggle("close");
 })
