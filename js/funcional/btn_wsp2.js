7538cf111c1c5c4ad1be3721e71a6d54503af6ca
Jhon Cruz IMM <jhonlcp1997>
2022-08-23 14:58:38 -0500

    add function btn wsp

---
+++ js/funcional/btn_wsp2.js
@@ -0,0 +1,17 @@
+const $btn_float = document.getElementById('btn_wsp');
+let width_page = window.innerWidth;
+let $screenWidth = screen.width;
+
+if($screenWidth <= 720){
+    $btn_float.setAttribute('href', "https://wa.me/51989266655");
+}
+
+window.addEventListener('resize', () => {
+    $screenWidth = screen.width;
+
+    if($screenWidth <= 720){
+        $btn_float.setAttribute('href', "https://wa.me/51989266655");
+    } else{
+        $btn_float.setAttribute('href', "https://web.whatsapp.com/send?phone=51989266655");
+    }
+})
