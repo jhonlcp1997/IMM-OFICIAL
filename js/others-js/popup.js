a628fddb9a10366a903ea8c0cfeee1563009bd3d
Jhon Cruz IMM <jhonlcp1997>
2022-09-02 14:06:00 -0500

    add new changes popup.js

--- js/others-js/popup.js
+++ js/others-js/popup.js
@@ -1,11 +1,19 @@
+function disableScroll() {
+    window.scrollTo(0, 0);
+}
+
+window.addEventListener('scroll', disableScroll);
+
 const $popup = document.querySelector(".popups");
 const $icon_close = document.getElementById("icon_close");
 const $btnClose = document.getElementById("btn_popup");
 
 $icon_close.addEventListener("click", () =>{
     $popup.classList.toggle("close");
+    window.removeEventListener('scroll', disableScroll); 
 })
 
 $btnClose.addEventListener("click", () =>{
     $popup.classList.toggle("close");
+    window.removeEventListener('scroll', disableScroll); 
 })
